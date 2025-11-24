4163e0f867bb57e7af778ecfa09863ea34056b7b
Kishore1803 <kannan.v4704@gmail.com>
2025-11-24 13:17:35 +0530

    m

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// signup page
+function signup() {
+    let name = document.getElementById("fullname").value;
+    let email = document.getElementById("email").value;
+    let pass = document.getElementById("password").value;
+    let cpass = document.getElementById("confirmPassword").value;
+
+    if (name === "" || email === "" || pass === "" || cpass === "") {
+        alert("Please fill all fields!");
+        return;
+    }
+
+    if (pass !== cpass) {
+        alert("Passwords do not match!");
+        return;
+    }
+    // Save data temporarily in localStorage (optional)
+    localStorage.setItem("netflixUser", email);
+    localStorage.setItem("netflixPass", pass);
+    alert("Account created successfully!");
+    window.location.href = "index.html";
+}
+
+// login page
 function login() {
     const username = document.getElementById("user").value;
     const password = document.getElementById("pass").value;
